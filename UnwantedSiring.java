89430b2426999fd9e7b23db6e0099e922bcc6ae7
Chetan7878 <50352274+Chetan7878@users.noreply.github.com>
2019-05-07 11:24:30 +0530

    Add files via upload

---
+++ UnwantedSiring.java
@@ -0,0 +1,15 @@
+import java.util.*;
+//import java.util.Scanner;
+class UnwantedSiring
+{ 
+ public static void main(String args[])
+{
+//Scanner s =new Scanner(System.in);
+ //System.out.println("Enter any String");
+  //String name=s.next();
+  String name="12345chetan";
+   System.out.println(name);
+  String replace =name.replaceAll("12345","raj");
+  System.out.println(replace);
+}
+}
